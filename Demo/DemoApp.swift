962d5c1a87db4eaf02b80ec07a25340f61a0297e
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2021-03-23 10:46:32 +0900

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by ياسين البدوي on 2021/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
